c6b9b8e3b4f6ef77f5a33c7a3262ca1a75bc20ea
Felipe Rodrigues <felipe@felipevr.com>
2020-01-31 17:55:26 -0300

    Add utilitary function to get URL for SRR IDs

---
+++ cov2019/utils.py
@@ -0,0 +1,71 @@
+"""
+tool box of useful functions
+"""
+import logging
+import os
+
+from dbvirus_searcher.searcher import Searcher
+
+
+def srr_to_url(srr):
+    """
+    Given an SRA SRR number, returns its URL.
+
+    In case of failure, it returns None.
+
+    Please provide an e-mail address in the NCBI_EMAIL environment variable. It
+    is not necessary, but it is a good practice.
+
+    Example of usage:
+    >>> srr_to_url("SRR042301")
+    'https://sra-download.st-va.ncbi.nlm.nih.gov/sos2/sra-pub-run-6/SRR042301/SRR042301.3'
+
+    >>> srr_to_url("invalid")
+    
+    >>> srr_to_url("042301")
+    'https://sra-download.st-va.ncbi.nlm.nih.gov/sos2/sra-pub-run-6/SRR042301/SRR042301.3'
+    """
+
+    srr = srr.upper()
+
+    email = os.environ.get("NCBI_EMAIL", "default@ncbi.com")
+
+    if email == "default@ncbi.com":
+        logging.warn(
+            "Please provide a custom email address by setting the NCBI_EMAIL env"
+        )
+
+    # If the SRR string does not start with SRR, we add it
+    if not srr.startswith("SRR"):
+        srr = f"SRR{srr}"
+
+    # Here we perform the basic SRA search
+    searcher = Searcher(email=email)
+    results = searcher.search(query=srr).get("esearchresult")
+    id_list = results.get("idlist")
+
+    if not id_list:
+        return None
+
+    id_ = id_list[0]
+    data = searcher.fetch(id_)
+
+    # We should not have more than 1 UID. Ever. Just in case we do, log it!
+    if len(id_list) > 1:
+        logging.warn(f"More than 1 UID found for {srr}")
+        logging.warn(f"Processing only the first UID, {id_}")
+    
+    # The result has a very complicated nested structure.
+    # Here we try to access the target data.
+    try:
+        run = data["EXPERIMENT_PACKAGE_SET"]["EXPERIMENT_PACKAGE"]["RUN_SET"]["RUN"]
+        sra_file = run["SRAFiles"]["SRAFile"]
+        url = sra_file["@url"]
+    except KeyError as e:
+        logging.error(f"Failed to acquire link for SRR {srr}")
+        logging.error(e)
+
+        return None
+    else:
+        return url
+
